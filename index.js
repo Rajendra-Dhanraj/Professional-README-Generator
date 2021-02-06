dcfc8b099b241d5742a29d866c381beb91abd648
Rajendra Dhanraj <rajiedhan@Rajendras-MacBook-Pro.local>
2021-02-06 16:16:00 -0500

    added bage and link

--- index.js
+++ index.js
@@ -65,13 +65,11 @@ const questions = [
     type: "input",
     name: "installation",
     message: "What command should be run to install dependencies?",
-    default: "npm i",
   },
   {
     type: "input",
     name: "test",
     message: "What command should be run to run tests?",
-    default: "npm test",
   },
   {
     type: "input",
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license == "None") {
@@ -9,34 +9,44 @@ function renderLicenseBadge(license) {
     return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
   } else if (license == "MIT") {
     return `![license: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
-  } else if ((license = "BSD 3")) {
+  } else if ((license == "BSD 3")) {
     return `![license](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)`;
-  };
-};
+  }
+}
 
-// TODO: Create a function that returns the license link
+//Create a Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license == "None") {
     return "";
   } else if (license == "APACHE 2.0") {
-    return `(https://opensource.org/licenses/Apache-2.0)`;
+    return `https://opensource.org/licenses/Apache-2.0`;
   } else if (license == "GPL 3.0") {
-    return `(https://www.gnu.org/licenses/gpl-3.0)`;
+    return `https://www.gnu.org/licenses/gpl-3.0`;
   } else if (license == "MIT") {
-    return `(https://opensource.org/licenses/MIT)`;
-  } else if ((license = "BSD 3")) {
-    return `(https://opensource.org/licenses/BSD-3-Clause)`;
-  };
-};
+    return `https://opensource.org/licenses/MIT`;
+  } else if ((license == "BSD 3")) {
+    return `https://opensource.org/licenses/BSD-3-Clause`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `${renderLicenseLink(license)}${license}`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return ` 
+
+  ${renderLicenseBadge(data.license)}
+
+
 # ${data.title}
 
   ## Table of Contents
@@ -61,7 +71,10 @@ function generateMarkdown(data) {
 ${data.usage}
 
 ### License
-  
+
+[Click here to view license details](${renderLicenseLink(data.license)})
+
+This application is covered under the ${data.license} license.
   
 ### Contributing
 
@@ -69,7 +82,7 @@ ${data.contributing}
 
 ### Tests
 
-${data.tests}
+${data.test}
 
 ### Questions
 

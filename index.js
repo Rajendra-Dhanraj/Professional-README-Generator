650d754292e9455ab85cde20f05c21168617885e
Rajendra Dhanraj <rajiedhan@Rajendras-MacBook-Pro.local>
2021-02-06 14:28:47 -0500

    passing user answers to generatemarkdown , added license badge and link, formatted readme

--- index.js
+++ index.js
@@ -36,31 +36,31 @@ const questions = [
     name: "title",
     message: "What is your project's name?",
     validate: (titleInput) => {
-        if (titleInput) {
-          return true;
-        } else {
-          console.log("Please enter your projects name!");
-        }
-      },
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter your projects name!");
+      }
+    },
   },
   {
     type: "input",
     name: "description",
     message: "Please write a short description of your project",
     validate: (descriptionInput) => {
-        if (descriptionInput) {
-          return true;
-        } else {
-          console.log("Please enter a short description of your project!");
-        }
-      },
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log("Please enter a short description of your project!");
+      }
+    },
   },
   {
     type: "list",
     name: "license",
     message: "What kind of license should your project have?",
     choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
-   },
+  },
   {
     type: "input",
     name: "installation",
@@ -78,33 +78,46 @@ const questions = [
     name: "usage",
     message: "What does the user need to know about using the repo?",
     validate: (usageInput) => {
-        if (usageInput) {
-          return true;
-        } else {
-          console.log("Please enter what the user needs to know, using the repo!");
-        }
-      },
+      if (usageInput) {
+        return true;
+      } else {
+        console.log(
+          "Please enter what the user needs to know, using the repo!"
+        );
+      }
+    },
   },
   {
     type: "input",
     name: "contributing",
     message: "What does the user need to know about contributing to the repo?",
     validate: (contributingInput) => {
-        if (contributingInput) {
-          return true;
-        } else {
-          console.log("Please enter what the user needs to know about contributing to the repo!");
-        }
-      },
+      if (contributingInput) {
+        return true;
+      } else {
+        console.log(
+          "Please enter what the user needs to know about contributing to the repo!"
+        );
+      }
+    },
   },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+
+// }
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(questions);
+  inquirer.prompt(questions).then((userAnwers) => {
+    const pageREADME = generateMarkdown(userAnwers);
+    fs.writeFile("./README.md", pageREADME, (err) => {
+      if (err) throw new Error(err);
+      console.log("Your README.md file has been created!");
+    });
+  });
 }
 // Function call to initialize app
+
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license == "None") {
+    return "";
+  } else if (license == "APACHE 2.0") {
+    return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
+  } else if (license == "GPL 3.0") {
+    return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
+  } else if (license == "MIT") {
+    return `![license: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  } else if ((license = "BSD 3")) {
+    return `![license](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)`;
+  };
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license == "None") {
+    return "";
+  } else if (license == "APACHE 2.0") {
+    return `(https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license == "GPL 3.0") {
+    return `(https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (license == "MIT") {
+    return `(https://opensource.org/licenses/MIT)`;
+  } else if ((license = "BSD 3")) {
+    return `(https://opensource.org/licenses/BSD-3-Clause)`;
+  };
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,9 +36,50 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return ` 
+# ${data.title}
 
-`;
+  ## Table of Contents
+  1. [Description](#description)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [License](#license)
+  5. [Contributing](#contributing)
+  6. [Tests](#tests)
+  7. [Questions](#questions)
+
+### Descrption
+
+  ${data.description}
+
+### Installation
+
+  ${data.installation}
+
+### Usage
+
+${data.usage}
+
+### License
+  
+  
+### Contributing
+
+${data.contributing}
+
+### Tests
+
+${data.tests}
+
+### Questions
+
+Github Username: ${data.github}
+
+[Click to view my Github Profile!](https://github.com/${data.github}) 
+
+Alternitively, I can be contacted via email: ${data.email}
+
+  `;
 }
 
 module.exports = generateMarkdown;
